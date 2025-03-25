f13781549e4ea690e474c9016d6f61e8f5b6b6da
Sanskriti <sanskritisri27@gmail.com>
2025-03-25 15:55:44 +0530

    added the top 20 books achieved by analysis

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, url_for
+import pandas as pandas
+import pickle
+
+
+top_20_books = pickle.load(open('top-20-books.pkl', 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", 
+                           book_name = list(top_20_books['Book-Title'].values),
+                           author=list(top_20_books['Book-Author'].values),
+                           image=list(top_20_books['Image-URL-M'].values),
+                           rating=list(top_20_books['avg-rating'].values))
 
 if __name__ == "__main__":
     app.run(debug=True)
